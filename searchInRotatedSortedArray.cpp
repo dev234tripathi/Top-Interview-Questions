b8b9f419788e4aeebd109fb21be8784d8191c318
Dev <dev234tripathi@gmail.com>
2020-03-10 14:57:31 +0530

    Add files via upload

---
+++ searchInRotatedSortedArray.cpp
@@ -0,0 +1,71 @@
+/**
+Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
+
+(i.e., [0,1,2,4,5,6,7] might become [4,5,6,7,0,1,2]).
+
+You are given a target value to search. If found in the array return its index, otherwise return -1.
+
+You may assume no duplicate exists in the array.
+
+Your algorithm's runtime complexity must be in the order of O(log n).
+
+Example 1:
+
+Input: nums = [4,5,6,7,0,1,2], target = 0
+Output: 4
+Example 2:
+
+Input: nums = [4,5,6,7,0,1,2], target = 3
+Output: -1
+*/
+
+//cpp Solution :
+
+ int search(vector<int>& nums, int target) {
+      int l = 0, r = nums.size()-1;
+    while (l<=r) {
+        int mid = (r-l)/2+l;
+        if (nums[mid] == target)
+            return mid;
+        if (nums[mid] < nums[r]) {
+            if (nums[mid]<target && target<=nums[r])
+                l = mid+1;
+            else
+                r = mid-1;
+        } else {
+            if(nums[l]<=target && target<nums[mid])
+                r = mid-1;
+            else
+                l = mid+1;
+        }
+    }
+    return -1;
+    }
+	
+//Java Solution : Binary Search
+
+/**
+ int search(int A[], int n, int target) {
+        int lo=0,hi=n-1;
+        // find the index of the smallest value using binary search.
+        // Loop will terminate since mid < hi, and lo or hi will shrink by at least 1.
+        // Proof by contradiction that mid < hi: if mid==hi, then lo==hi and loop would have been terminated.
+        while(lo<hi){
+            int mid=(lo+hi)/2;
+            if(A[mid]>A[hi]) lo=mid+1;
+            else hi=mid;
+        }
+        // lo==hi is the index of the smallest value and also the number of places rotated.
+        int rot=lo;
+        lo=0;hi=n-1;
+        // The usual binary search and accounting for rotation.
+        while(lo<=hi){
+            int mid=(lo+hi)/2;
+            int realmid=(mid+rot)%n;
+            if(A[realmid]==target)return realmid;
+            if(A[realmid]<target)lo=mid+1;
+            else hi=mid-1;
+        }
+        return -1;
+    }
+*/
