1084a23bd6247a52c5b4b080c96ac4f495e5ccd3
Dev <dev234tripathi@gmail.com>
2020-03-10 09:29:15 +0530

    Add files via upload

---
+++ twoSum.java
@@ -0,0 +1,51 @@
+/**
+Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+Example:
+
+Given nums = [2, 7, 11, 15], target = 9,
+
+Because nums[0] + nums[1] = 2 + 7 = 9,
+return [0, 1].
+*/
+
+public int[] twoSum(int[] numbers, int target) {
+    int[] result = new int[2];
+    Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+    for (int i = 0; i < numbers.length; i++) {
+        if (map.containsKey(target - numbers[i])) {
+            result[1] = i;
+            result[0] = map.get(target - numbers[i]);
+            return result;
+        }
+        map.put(numbers[i], i);
+    }
+    return result;
+}
+
+
+
+/**
+Assumption in previous problem : Duplicates doesn't exists. But if it exists, then few changes are required.
+*/
+public int[] twoSum(int[] numbers, int target) {
+    int[] result = new int[2];
+    Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+    for (int i = 0; i < numbers.length; i++) {
+        if (map.containsKey(target - numbers[i])) {
+            result[1] = i + 1;//check i
+            result[0] = map.get(target - numbers[i]);
+            return result;
+        }
+
+        //the code lines below avoids the "duplicate keys" problem
+        if(map.containsKey(numbers[i])) {
+           continue;
+        }
+
+        map.put(numbers[i], i + 1);//here too
+    }
+    return result;
+}
