e6766e04442ce6f16ed73c4404bca7c0628e80c6
Dev <dev234tripathi@gmail.com>
2020-03-10 13:34:51 +0530

    Add files via upload

---
+++ letterCombinationOfAPhoneNumber.cpp
@@ -0,0 +1,54 @@
+/**
+Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could represent.
+
+A mapping of digit to letters (just like on the telephone buttons) is given below. Note that 1 does not map to any letters.
+
+Example:
+
+Input: "23"
+Output: ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
+Note:
+
+Although the above answer is in lexicographical order, your answer could be in any order you want.
+*/
+
+
+//cpp Solution : 
+vector<string> letterCombinations(string digits) {
+    vector<string> res;
+    string charmap[10] = {"0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+    res.push_back("");
+    for (int i = 0; i < digits.size(); i++)
+    {
+        vector<string> tempres;
+        string chars = charmap[digits[i] - '0'];
+        for (int c = 0; c < chars.size();c++)
+            for (int j = 0; j < res.size();j++)
+                tempres.push_back(res[j]+chars[c]);
+        res = tempres;
+    }
+    return res;
+}
+
+/**
+
+This is a iterative solution. For each digit added, remove and copy every element in the queue and add the possible letter to each element, 
+then add the updated elements back into queue again. Repeat this procedure until all the digits are iterated.
+
+java Solution :
+
+public List<String> letterCombinations(String digits) {
+		LinkedList<String> ans = new LinkedList<String>();
+		if(digits.isEmpty()) return ans;
+		String[] mapping = new String[] {"0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+		ans.add("");
+		while(ans.peek().length()!=digits.length()){
+			String remove = ans.remove();
+			String map = mapping[digits.charAt(remove.length())-'0'];
+			for(char c: map.toCharArray()){
+				ans.addLast(remove+c);
+			}
+		}
+		return ans;
+	}
+*/
