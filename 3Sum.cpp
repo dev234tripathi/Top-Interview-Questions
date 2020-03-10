f9d52c5f3cf3b50a0853f0db57305f2b02a8883d
Dev <dev234tripathi@gmail.com>
2020-03-10 12:16:52 +0530

    Add files via upload

---
+++ 3Sum.cpp
@@ -0,0 +1,71 @@
+/**
+Given an array nums of n integers, are there elements a, b, c in nums such that a + b + c = 0? Find all unique triplets in the array which gives the sum of zero.
+
+Note:
+
+The solution set must not contain duplicate triplets.
+
+Example:
+
+Given array nums = [-1, 0, 1, 2, -1, -4],
+
+A solution set is:
+[
+  [-1, 0, 1],
+  [-1, -1, 2]
+]
+*/
+
+//cpp Solution :
+
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+         sort(nums.begin(), nums.end());
+    vector<vector<int>> res;
+    for (unsigned int i=0; i<nums.size(); i++) {
+        if ((i>0) && (nums[i]==nums[i-1]))
+            continue;
+        int l = i+1, r = nums.size()-1;
+        while (l<r) {
+            int s = nums[i]+nums[l]+nums[r];
+            if (s>0) r--;
+            else if (s<0) l++;
+            else {
+                res.push_back(vector<int> {nums[i], nums[l], nums[r]});
+                while (l + 1 < i && nums[l]==nums[l+1]) l++;
+                while (r - 1 > l && nums[r]==nums[r-1]) r--;
+                l++; r--;
+            }
+        }
+    }
+    return res;
+    }
+};
+
+//java Solution :
+/**
+The idea is to sort an input array and then run through all indices of a possible first element of a triplet. 
+For each possible first element we make a standard bi-directional 2Sum sweep of the remaining part of the array. 
+Also we want to skip equal elements to avoid duplicates in the answer without making a set or smth like that.
+
+public List<List<Integer>> threeSum(int[] num) {
+    Arrays.sort(num);
+    List<List<Integer>> res = new LinkedList<>(); 
+    for (int i = 0; i < num.length-2; i++) {
+        if (i == 0 || (i > 0 && num[i] != num[i-1])) {
+            int lo = i+1, hi = num.length-1, sum = 0 - num[i];
+            while (lo < hi) {
+                if (num[lo] + num[hi] == sum) {
+                    res.add(Arrays.asList(num[i], num[lo], num[hi]));
+                    while (lo < hi && num[lo] == num[lo+1]) lo++;
+                    while (lo < hi && num[hi] == num[hi-1]) hi--;
+                    lo++; hi--;
+                } else if (num[lo] + num[hi] < sum) lo++;
+                else hi--;
+           }
+        }
+    }
+    return res;
+}
+*/
