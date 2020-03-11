1cb075b7bfeb7b12dcf359a28ce0003e205c6e85
Dev <dev234tripathi@gmail.com>
2020-03-11 11:43:46 +0530

    Add files via upload

---
+++ mergeIntervals.java
@@ -0,0 +1,43 @@
+/**
+Given a collection of intervals, merge all overlapping intervals.
+
+Example 1:
+
+Input: [[1,3],[2,6],[8,10],[15,18]]
+Output: [[1,6],[8,10],[15,18]]
+Explanation: Since intervals [1,3] and [2,6] overlaps, merge them into [1,6].
+Example 2:
+
+Input: [[1,4],[4,5]]
+Output: [[1,5]]
+Explanation: Intervals [1,4] and [4,5] are considered overlapping.
+*/
+//java version :
+/**
+The idea is to sort the intervals by their starting points. Then, 
+we take the first interval and compare its end with the next intervals starts. 
+As long as they overlap, we update the end to be the max end of the overlapping intervals. 
+Once we find a non overlapping interval, we can add the previous "extended" interval and start over.
+
+Sorting takes O(n log(n)) and merging the intervals takes O(n). So, the resulting algorithm takes O(n log(n)).
+*/
+
+	if (intervals.length <= 1)
+			return intervals;
+
+		// Sort by ascending starting point
+		Arrays.sort(intervals, (i1, i2) -> Integer.compare(i1[0], i2[0]));
+
+		List<int[]> result = new ArrayList<>();
+		int[] newInterval = intervals[0];
+		result.add(newInterval);
+		for (int[] interval : intervals) {
+			if (interval[0] <= newInterval[1]) // Overlapping intervals, move the end if needed
+				newInterval[1] = Math.max(newInterval[1], interval[1]);
+			else {                             // Disjoint intervals, add the new interval to the list
+				newInterval = interval;
+				result.add(newInterval);
+			}
+		}
+
+		return result.toArray(new int[result.size()][]);
