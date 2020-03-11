6dab3f4202f60d38e95c9f0719ec87a1390e9795
Dev <dev234tripathi@gmail.com>
2020-03-11 18:15:04 +0530

    Add files via upload

---
+++ binaryTreeMaximumSumPath.java
@@ -0,0 +1,71 @@
+/**
+Given a non-empty binary tree, find the maximum path sum.
+
+For this problem, a path is defined as any sequence of nodes from some starting node to any node in the tree along the parent-child connections. The path must contain at least one node and does not need to go through the root.
+
+Example 1:
+
+Input: [1,2,3]
+
+       1
+      / \
+     2   3
+
+Output: 6
+Example 2:
+
+Input: [-10,9,20,null,null,15,7]
+
+   -10
+   / \
+  9  20
+    /  \
+   15   7
+
+Output: 42
+*/
+
+
+/**
+A path from start to end, goes up on the tree for 0 or more steps,
+then goes down for 0 or more steps. Once it goes down, it can't go up. 
+Each path has a highest node, which is also the lowest common ancestor of all other nodes on the path.
+A recursive method maxPathDown(TreeNode node) (1) computes the maximum path sum with highest node is the 
+input node, update maximum if necessary (2) returns the maximum sum of the path that can be extended to input node's parent.
+
+
+The most tricky point is the global variable maxValue in the following sentence:
+
+maxValue = Math.max(maxValue, left + right + node.val);
+The second maxValue contains the bigger between the left sub-tree and right sub-tree.
+if (left + right + node.val < maxValue ) then the result will not include the parent node which means 
+the maximum path is in the left branch or right branch
+
+
+maxValue is the value which recording whether this current root is the final root, so we use left + right + node.val. But to the upper layer(after return statement), we cannot choose both left and right brunches, 
+so we need to select the larger one, so we use max(left, right) + node.val to prune the lower brunch.
+*/
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    int maxValue;
+    public int maxPathSum(TreeNode root) {
+        maxValue = Integer.MIN_VALUE;
+        helper(root);
+        return maxValue;
+    }
+    public int helper(TreeNode node) {
+        if(node == null) return 0;
+        int left = Math.max(0,helper(node.left));
+        int right = Math.max(0,helper(node.right));
+        maxValue = Math.max(maxValue,left+right+node.val);
+        return Math.max(left,right)+node.val;
+    } 
+}
