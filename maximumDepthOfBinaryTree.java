dc9e660652fdd9cf01236a5069091d7b90340aeb
Dev <dev234tripathi@gmail.com>
2020-03-11 17:05:14 +0530

    Add files via upload

---
+++ maximumDepthOfBinaryTree.java
@@ -0,0 +1,56 @@
+/**
+Given a binary tree, find its maximum depth.
+
+The maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+
+Note: A leaf is a node with no children.
+
+Example:
+
+Given binary tree [3,9,20,null,null,15,7],
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+return its depth = 3.
+*/
+//cpp version : (recursive) 
+ int maxDepth(TreeNode* root) {
+        if(root==NULL)
+            return 0;
+        int lLength = maxDepth(root->left);
+        int rLength= maxDepth(root->right);
+        if(lLength>rLength)
+            return lLength+1;
+        else
+            return rLength+1;
+    }
+	
+//non recursive : 
+int maxDepth(TreeNode *root)
+{
+    if(root == NULL)
+        return 0;
+    
+    int res = 0;
+    queue<TreeNode *> q;
+    q.push(root);
+    while(!q.empty())
+    {
+        ++ res;
+        for(int i = 0, n = q.size(); i < n; ++ i)
+        {
+            TreeNode *p = q.front();
+            q.pop();
+            
+            if(p -> left != NULL)
+                q.push(p -> left);
+            if(p -> right != NULL)
+                q.push(p -> right);
+        }
+    }
+    
+    return res;
+}
